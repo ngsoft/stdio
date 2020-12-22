a3313e352ea37101a4ad119b65d8fd068d08c163
Aymeric Anger <daedelus.git@gmail.com>
2020-12-22 10:56:53 +0100

    base refactor

--- src/STDIO.php
+++ src/STDIO.php
@@ -3,7 +3,8 @@
 namespace NGSOFT;
 
 use NGSOFT\STDIO\{
-    Inputs\StreamInput, Interfaces\Buffer, Outputs\OutputBuffer, Outputs\StreamOutput, Terminal
+    Inputs\StreamInput, Interfaces\Buffer, Interfaces\Formatter, Interfaces\Input, Interfaces\Output, Outputs\OutputBuffer,
+    Outputs\StreamOutput, Styles, Terminal
 };
 
 class STDIO {
@@ -25,6 +26,9 @@ class STDIO {
     /** @var Styles */
     private $styles;
 
+    /** @var Formatter */
+    private $formatter;
+
     public function __construct() {
         $this->terminal = new Terminal();
         $this->inputs['in'] = new StreamInput();
@@ -32,7 +36,74 @@ class STDIO {
         $this->outputs['out'] = $stdout;
         $this->outputs['err'] = $stdout->withStream(fopen('php://stderr', 'w'));
         $this->buffer = new OutputBuffer();
-        $this->styles = STDIO\Styles::create();
+        $this->styles = Styles::create();
+    }
+
+    ////////////////////////////   GETTERS   ////////////////////////////
+
+    /**
+     * Get Terminal Infos
+     * @return Terminal
+     */
+    public function getTerminal(): Terminal {
+        return $this->terminal;
+    }
+
+    /**
+     * Get Inputs
+     * @return array<string,STDIO\Interfaces\Input>
+     */
+    public function getInputs() {
+        return $this->inputs;
+    }
+
+    /**
+     * Get Outputs
+     * @return array<string,STDIO\Interfaces\Output>
+     */
+    public function getOutputs() {
+        return $this->outputs;
+    }
+
+    /**
+     * Get Buffer
+     * @return Buffer
+     */
+    public function getBuffer(): Buffer {
+        return $this->buffer;
+    }
+
+    /**
+     * Get Styles
+     * @return Styles
+     */
+    public function getStyles(): Styles {
+        return $this->styles;
+    }
+
+    /**
+     * Ger Current Formatter
+     * @return Formatter
+     */
+    public function getFormatter(): Formatter {
+        return $this->formatter;
+    }
+
+    /**
+     * Get Input by name
+     * @return Input|null
+     */
+    public function getInput(string $index = 'in'): ?Input {
+        return $this->inputs[$index] ?? null;
+    }
+
+    /**
+     * Get Output by name
+     * @param string $index
+     * @return Output|null
+     */
+    public function getOutput(string $index = 'out'): ?Output {
+        return $this->outputs[$index] ?? null;
     }
 
 }
--- src/STDIO/Styles/Style.php
+++ src/STDIO/Styles/Style.php
@@ -9,11 +9,41 @@ use NGSOFT\STDIO\{
 class Style implements Formatter {
 
     private $prefix = [];
-    private $suffix = [Styles::STYLE_RESET];
+    private $suffix = [];
+
+    /** @return \NGSOFT\STDIO\Styles\Style */
+    private function getClone(): Style {
+        return clone $this;
+    }
+
+    /**
+     * Get a clone with defined prefix
+     * @param array $codes
+     * @return \NGSOFT\STDIO\Styles\Style
+     */
+    public function withPrefix(array $codes): Style {
+        $clone = $this->getClone();
+        $clone->prefix = $codes;
+        return $clone;
+    }
+
+    /**
+     * Get a clone with defined suffix
+     * @param array $codes
+     * @return \NGSOFT\STDIO\Styles\Style
+     */
+    public function withSuffix(array $codes): Style {
+        $clone = $this->getClone();
+        $clone->suffix = $codes;
+        return $clone;
+    }
 
     /** {@inheritdoc} */
     public function format(string $message): string {
-        return $message;
+        $prefix = $suffix = '';
+        if (count($this->prefix) > 0) $prefix = sprintf(Styles::ESCAPE . '%s' . Styles::STYLE_SUFFIX, implode(';', $this->prefix));
+        if (count($this->suffix) > 0) $suffix = sprintf(Styles::ESCAPE . '%s' . Styles::STYLE_SUFFIX, implode(';', $this->prefix));
+        return sprintf("%s%s%s", $prefix, $message, $suffix);
     }
 
 }
